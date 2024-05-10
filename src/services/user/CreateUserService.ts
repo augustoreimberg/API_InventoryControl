3d63d4247d3fd455e69f001d615658127605df02
Augusto Remberg <141145533+augustoreimberg@users.noreply.github.com>
2024-05-10 15:37:50 -0300

    creation of the service to create a user and executing it by creating a controller

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,13 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import prismaClient from "../../prisma";
 import { hash } from "bcryptjs";
 import { UserRequest } from "../../models/interfaces/user/UserRequest";
-import prismaClient from "../../prisma";
 
 class CreateUserService{
 
     async execute({ name, email, password }: UserRequest) {
         if (!email) {
-            throw new Error("Email incorrect";)
+            throw new Error("Email incorrect")
         }
 
         const userAlreadyExists = await prismaClient.user.findFirst({
@@ -22,6 +21,21 @@ class CreateUserService{
 
         //encrypting user password
         const passwordHash = await hash(password, 8);
+
+        const user = prismaClient.user.create({
+            data: {
+                name: name,
+                email: email,
+                password: passwordHash
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true
+            }
+        })
+
+        return user;
     }
 }
 
