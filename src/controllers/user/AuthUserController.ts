0cb9966ef128785ced31922ed4d4bd46b4dce1c3
Augusto Remberg <141145533+augustoreimberg@users.noreply.github.com>
2024-05-12 11:53:00 -0300

    creating controller for user authentication

--- src/controllers/user/AuthUserController.ts
+++ src/controllers/user/AuthUserController.ts
@@ -1,1 +1,16 @@
 import { Request, Response } from "express";
+import { AuthUserService } from "../../services/user/AuthUserService";
+import { AuthRequest } from "../../models/interfaces/user/auth/AuthRequest";
+
+class authUserController {
+    async handle(request: Request, response: Response) {
+        const { email, password }: AuthRequest = request.body;
+        const authUserService = new AuthUserService();
+        const auth = await authUserService.execute({
+            email, password
+        })
+        return response.json(auth)
+    }
+}
+
+export { authUserController };
