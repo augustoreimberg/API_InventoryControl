05b6b3ef832b3a437e3c33fe590e3d4ff0fdcb9f
Augusto Remberg <141145533+augustoreimberg@users.noreply.github.com>
2024-06-17 10:39:35 -0300

    Implementing rote to create category

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { AuthUserController } from "./controllers/user/AuthUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { RemoveUserController } from "./controllers/user/RemoveUserController";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 
 const router = Router();
 
@@ -15,5 +16,8 @@ router.post("/authUser", new AuthUserController().handle);
 router.get("/getUserById", isAuthenticated, new DetailUserController().handle);
 
 router.delete("/user/remove", new RemoveUserController().handle)
+
+//Category Routs
+router.post("/createCategory", isAuthenticated, new CreateCategoryController().handle)
 
 export { router };
